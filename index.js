915cc8e7e91f65b7b661e4233e9f2ce3b1f9f9dc
Melissa Brazle <melissaabrazle@outlook.com>
2023-03-18 02:37:31 -0500

    modified index.js & added package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, value) {
+    
+    const surpriseCard = [];
+
+ for (let i = 0; i < names.length; i++)  {
+        surpriseCard.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    };
+  return surpriseCard;
+};
+
+function countDown() {
+    let countNumber = 10;
+while (countNumber >= 0) {
+  console.log(countNumber--);
+}
+}
+
+
+
+
+
+
+
+
+
